a3af4f25657fda031970eb09c5f00411a54ef21f
mjdough <doughmj@outlook.com>
2016-03-11 10:32:42 +0000

    added destroy, update, show, and cleaned up duplicate codes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,16 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+
   end
 
   def create
@@ -13,11 +23,32 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @article = Article.find(params[:id])
+
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
   end
 
   private
+
+    def set_article
+       @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
