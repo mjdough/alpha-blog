1b39ac2015061651600391a0f9ffad04ef16efe4
docmj <doughmj@outlook.com>
2016-04-02 16:30:16 +0000

    Last changes to update and fix

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,12 +1,13 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+    
     def setup
         @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
     end
     
     test "get new category form and create category" do
-        sign_in_as(@user, "john")
+        sign_in_as(@user, "password")
         get new_category_path
         assert_template 'categories/new'
         assert_difference 'Category.count', 1 do
