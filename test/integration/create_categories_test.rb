d1aef674bd780f0227269e48585875ef470a39cd
docmj <doughmj@outlook.com>
2016-04-03 00:14:22 +0000

    add edit category name for admins

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -18,7 +18,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
     
     test "invalid category submission results in failure" do
-        sign_in_as(@user, "john")
+        sign_in_as(@user, "password")
         get new_category_path
         assert_template 'categories/new'
         assert_no_difference 'Category.count' do
